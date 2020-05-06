f1fc2199dee71cb7f95f87ff955f9b8b629ac9c7
Tanner <tsutton720@gmail.com>
2020-05-06 16:31:30 -0700

    Formating

--- GUI.py
+++ GUI.py
@@ -25,13 +25,13 @@ class button():
         if self.text != '':
             font = pygame.font.SysFont('genevattf', 25)
             text = font.render(self.text, 1, (0, 0, 0))
-            win.blit(text, (self.x + (self.width/2 - text.get_width()/2),
-                            self.y + (self.height/2 - text.get_height()/2)))
+            win.blit(text, (self.x + (self.width//2 - text.get_width()//2),
+                            self.y + (self.height//2 - text.get_height()//2)))
 
     def isOver(self, pos):
         # Pos is the mouse position or a tuple of (x,y) coordinates
-        if pos[0] > self.x and pos[0] < self.x + self.width:
-            if pos[1] > self.y and pos[1] < self.y + self.height:
+        if self.x < pos[0] < self.x + self.width:
+            if self.y < pos[1] < self.y + self.height:
                 return True
 
         return False
